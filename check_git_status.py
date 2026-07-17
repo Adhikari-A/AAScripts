211fd9cc297b6a884cd7916b473d3ad588afaef6
Ananya Adhikari <ananya.adhikari@tecnico.ulisboa.pt>
2026-07-17 16:22:55 +0100

    updated patch file naming in check_git_status.py

--- check_git_status.py
+++ check_git_status.py
@@ -20,7 +20,7 @@ tokens = (str(datetime.datetime.now())).split()
 tokens[1] = tokens[1].split('.')[0]
 # cur_time = tokens[0].replace('-','')+tokens[1].replace(':','')
 cur_time = tokens[0]+"."+tokens[1].replace(':','-')
-name = f"diff_{cur_time}.patch"
+common_piece = f"diff_{cur_time}.patch"
 
 def perform_operations(d):
 
@@ -42,6 +42,7 @@ def perform_operations(d):
   # os.system("git rev-parse HEAD")
   os.system("git log -1 | head -5")
   print()
+  name = f"{d}_{common_piece}"
   print(f"Creating diff patch: {name}")
   os.system(f"git diff > {name}")
   patch_file = Path(name)
